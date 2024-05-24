edcc28ed103a66619193fb0b25e07577f6bf378e
Kittipakorn <kittipakorn1234@gmail.com>
2024-05-24 19:45:35 +0700

    ver.1

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
 }
 
+document.getElementById('0').addEventListener('click', function() {
+    scrollToSection('home');
+});
+
 document.getElementById('1').addEventListener('click', function() {
     scrollToSection('page1');
 });
@@ -12,4 +16,16 @@ document.getElementById('2').addEventListener('click', function() {
 
 document.getElementById('3').addEventListener('click', function() {
     scrollToSection('page3');
+});
+
+document.getElementById('4').addEventListener('click', function() {
+    scrollToSection('page4')
+});
+
+document.getElementById('5').addEventListener('click', function() {
+    scrollToSection('page5');
+});
+
+document.getElementById('6').addEventListener('click', function() {
+    scrollToSection('aboutus');
 });
