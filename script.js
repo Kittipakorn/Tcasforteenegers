260e25651d04e1974a0c090237124d093c3b3ba4
Kittipakorn <kittipakorn1234@gmail.com>
2024-05-25 22:13:04 +0700

    new

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ window.addEventListener("scroll",function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+function toggleHam(x) {
+    x.classList.toggle("change");
+    var myMenu = document.getElementById("myMenu");
+    if (myMenu.className === "menu") {
+        myMenu.className += " menu-active";
+    }
+    else {
+        myMenu.className = "menu";
+    }
+  }
