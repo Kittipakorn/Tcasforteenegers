bb2eb9e594dfeb9cd2fe4942d5b33ce6b708f0ee
Kittipakorn <kittipakorn1234@gmail.com>
2024-05-25 18:10:07 +0700

    linkt to github,navbar,donation

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
-function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-}
-
-document.getElementById('0').addEventListener('click', function() {
-    scrollToSection('home');
-});
-
-document.getElementById('1').addEventListener('click', function() {
-    scrollToSection('page1');
-});
-
-document.getElementById('2').addEventListener('click', function() {
-    scrollToSection('page2');
-});
-
-document.getElementById('3').addEventListener('click', function() {
-    scrollToSection('page3');
-});
-
-document.getElementById('4').addEventListener('click', function() {
-    scrollToSection('page4')
-});
-
-document.getElementById('5').addEventListener('click', function() {
-    scrollToSection('page5');
-});
-
-document.getElementById('6').addEventListener('click', function() {
-    scrollToSection('aboutus');
-});
+window.addEventListener("scroll",function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
