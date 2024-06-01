3d78e63f92bb41153e8f2d37a597bd230c38c715
Kittipakorn <kittipakorn1234@gmail.com>
2024-06-01 18:09:06 +0700

    show members

--- script.js
+++ script.js
@@ -25,3 +25,43 @@ function toggleHam(x) {
         window.scrollTo(0, tmp-40);
     }
   }
+
+const carousel = document.querySelector(".carousel");
+const FirstCardWidth = carousel.querySelector(".card").offsetWidth;
+let isDragging = false,startX,startScrollLeft;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const arrowBtns = document.querySelectorAll(".wrapper i");
+
+    if (arrowBtns && arrowBtns.length > 0) {
+        arrowBtns.forEach(btn => {
+            btn.addEventListener("click", () => {
+                carousel.scrollLeft += btn.id === "left" ? -FirstCardWidth : FirstCardWidth;
+            });
+        });
+    } else {
+        console.error('No arrow buttons found or arrowBtns is undefined');
+    }
+});
+
+
+const draggStart = (e) => {
+    isDragging = true;  
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const draggStop = () => {
+    isDragging = false;  
+    carousel.classList.remove("dragging");
+}
+
+const dragging = (e) => {
+    if(!isDragging) return;
+    carousel.scrollLeft = startScrollLeft  - (e.pageX-startX);
+}
+
+carousel.addEventListener("mousedown",draggStart);
+carousel.addEventListener("mousemove",dragging);
+document.addEventListener("mouseup",draggStop);
